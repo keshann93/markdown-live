07cbc41e372c31659bc89fc81d4165808e0752cc
keshan <keshan011@live.com>
2020-05-24 16:05:25 +0530

    more updates

---
+++ ui/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import TuiEditor from './TuiEditor';
+
+ReactDOM.render(
+  <div>
+    <TuiEditor />
+  </div>,
+  document.getElementById('root')
+);
